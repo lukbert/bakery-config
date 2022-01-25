9f32fe41029d3d562b68a820f756131346baa44f
Luk <lucaszancanbertollo@gmail.com>
2022-01-25 09:35:15 -0300

    Initial commit

---
+++ src/test/java/com/bakery/nona/config/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakery.nona.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
